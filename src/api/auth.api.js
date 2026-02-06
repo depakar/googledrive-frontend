48e3b3105a0c9f4e10be452b1acfb8daa30c8e6f
depakar <depakar19@gmail.com>
2026-02-06 13:31:09 +0530

    Fix Netlify SPA routing

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "./axios.js";
 
 export const loginApi = async (data) => {
-  const res = await axios.post("/auth/login", data);
+  const res = await axios.post("/api/auth/login", data);
   return res.data;
 };
